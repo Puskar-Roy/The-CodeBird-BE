8ba2722ce417a706d986d96ae3c64aefc5e213b3
Puskar-Roy <puskarroy600@gmail.com>
2025-02-16 16:41:51 +0530

    fix : jwt secret

--- src/util/utils.ts
+++ src/util/utils.ts
@@ -1,12 +1,17 @@
 import jwt from 'jsonwebtoken';
 import config from '../config/config';
 import { randomInt } from 'crypto';
-import { Resend } from 'resend';
 
 export const createToken = (_id: string) => {
-  return jwt.sign({ _id: _id }, config.JWT_SECRET, {
-    expiresIn: config.JWT_COOKIE_EXPIRES_IN,
-  });
+  if (!_id) return;
+
+  const secret: string = config.JWT_SECRET as string;
+
+  if (!secret) {
+    throw new Error('JWT_SECRET is not defined in config');
+  }
+
+  return jwt.sign({ _id }, secret, { expiresIn: '3d' });
 };
 
 export const generateOTP = (): string => {
